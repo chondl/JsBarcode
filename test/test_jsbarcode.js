0194daea3c750d22114fbcb6856989125be14f53
Chris Hondl <chondl@cs.stanford.edu>
2013-02-10 11:09:41 -0800

    Add test for specifiying quiet width

--- test/test_jsbarcode.js
+++ test/test_jsbarcode.js
@@ -42,3 +42,18 @@ exports.testRendererRender = function(beforeExit, assert) {
   assert.eql(ctx.calls[18], [54,0,1,1])
   assert.eql(ctx.calls[19], [55,0,1,1])
 }
+
+exports.testRendererRenderWithSpecifiedQuietWidth = function(beforeExit, assert) {
+  var ctx = { calls: [], fillRect: function(x,y,w,h) { this.calls.push(Array.prototype.slice.call(arguments)) } }
+
+  assert.strictEqual(new jsbarcode.renderer('A', {format:"CODE128"}).render(ctx,0,0,46,1, 0), undefined)
+  assert.strictEqual(ctx.calls.length, 20)
+  //  1101001000010100011000100010110001100011101011
+  assert.eql(ctx.calls[0], [0,0,1,1])
+  assert.eql(ctx.calls[1], [1,0,1,1])
+  assert.eql(ctx.calls[2], [3,0,1,1])
+  assert.eql(ctx.calls[3], [6,0,1,1])
+  assert.eql(ctx.calls[17], [42,0,1,1])
+  assert.eql(ctx.calls[18], [44,0,1,1])
+  assert.eql(ctx.calls[19], [45,0,1,1])
+}
