e7d88c327c7f97890c157d7f117a613c6b4ab84a
Chris Hondl <chondl@cs.stanford.edu>
2013-02-10 10:31:05 -0800

    Add wrapper to render to a simple graphics context

--- main.js
+++ main.js
@@ -1,12 +1,53 @@
 ;(function(exports){
 
+  var encoders = {}
+
   function exportEncoder(module, name) {
-    if (require && !exports[name]) exports[name] = require(module)[name]
+    if (require && !exports[name]) encoders[name] = exports[name] = require(module)[name]
   }
 
   exportEncoder('./EAN_UPC.js', 'EAN')
   exportEncoder('./EAN_UPC.js', 'UPC')
   exportEncoder('./CODE128.js', 'CODE128')
+
+  exports.renderer = function(content, options) {
+    var encoder = encoders[options.format] && new encoders[options.format](content);
+
+    this._content = content;
+    this._options = options;
+		this._valid = !!encoder && encoder.valid();
+    this._encoded = this._valid && encoder.encoded();
+
+    this.valid = function() {
+      return this._valid;
+    }
+    
+    this.length = function() {
+      return this._encoded ? this._encoded.length : 0
+    }
+
+    this.width = function(barWidth, quietWidth) {
+      return this.length() * barWidth + 2 * quietWidth
+    }
+
+    this.render = function(ctx,x,y,w,h,quiet) {
+      var length = this.length(),
+          encoded = this._encoded,
+          quietWidth = quiet === undefined ? (10*w)/(length+20) : quiet,  
+          renderWidth = w - 2*quietWidth,
+          barWidth = renderWidth/length,
+          i, x
+
+		  for(i=0;i<length;i++){
+		  	if(encoded[i] == "1"){
+		    	ctx.fillRect((i/length)*renderWidth+quietWidth,y,barWidth,h);
+		  	}
+		  }
+
+    }
+
+    return this
+  }
 
 })(typeof exports === 'undefined' ? this['JsBarcode']=(this['JsBarcode'] || {}) : exports)
 ;
--- test/test_jsbarcode.js
+++ test/test_jsbarcode.js
@@ -4,12 +4,41 @@ var jsbarcode = require('../main.js')
 
 exports.testEAN = function(beforeExit, assert) {
   var barcode = new jsbarcode.EAN('9780199532179')
-  assert.eql(barcode.valid(), true)
-  assert.eql(barcode.encoded(), '10101110110001001010011100110010010111000101101010100111010000101101100110011010001001110100101')
+  assert.strictEqual(barcode.valid(), true)
+  assert.strictEqual(barcode.encoded(), '10101110110001001010011100110010010111000101101010100111010000101101100110011010001001110100101')
 }
 
 exports.testCODE128 = function(beforeExit, assert) {
   var barcode = new jsbarcode.CODE128('0#%Ab')
-  assert.eql(barcode.valid(), true)
-  assert.eql(barcode.encoded(), "110100100001001110110010010011000100010011001010001100010010000110111100100101100011101011")
+  assert.strictEqual(barcode.valid(), true)
+  assert.strictEqual(barcode.encoded(), "110100100001001110110010010011000100010011001010001100010010000110111100100101100011101011")
+}
+
+exports.testRendererValid = function(beforeExit, assert) {
+  assert.strictEqual(new jsbarcode.renderer('AKALK2#$', {format:"CODE128"}).valid(), true)
+  assert.strictEqual(new jsbarcode.renderer("\u20AC", {format:"CODE128"}).valid(), false)
+  assert.strictEqual(new jsbarcode.renderer('9780199532179', {format:"EAN"}).valid(), true)
+  assert.strictEqual(new jsbarcode.renderer('AKXK', {format:"EAN"}).valid(), false)
+}
+
+exports.testRendererLength = function(beforeExit, assert) {
+  assert.strictEqual(new jsbarcode.renderer('AKALK2#$', {format:"CODE128"}).length(), 123)
+  assert.strictEqual(new jsbarcode.renderer("\u20AC", {format:"CODE128"}).length(), 0)
+  assert.strictEqual(new jsbarcode.renderer('9780199532179', {format:"EAN"}).length(), 95)
+  assert.strictEqual(new jsbarcode.renderer('AKXK', {format:"EAN"}).length(), 0)
+}
+
+exports.testRendererRender = function(beforeExit, assert) {
+  var ctx = { calls: [], fillRect: function(x,y,w,h) { this.calls.push(Array.prototype.slice.call(arguments)) } }
+
+  assert.strictEqual(new jsbarcode.renderer('A', {format:"CODE128"}).render(ctx,0,0,66,1), undefined)
+  assert.strictEqual(ctx.calls.length, 20)
+  //  1101001000010100011000100010110001100011101011
+  assert.eql(ctx.calls[0], [10,0,1,1])
+  assert.eql(ctx.calls[1], [11,0,1,1])
+  assert.eql(ctx.calls[2], [13,0,1,1])
+  assert.eql(ctx.calls[3], [16,0,1,1])
+  assert.eql(ctx.calls[17], [52,0,1,1])
+  assert.eql(ctx.calls[18], [54,0,1,1])
+  assert.eql(ctx.calls[19], [55,0,1,1])
 }
